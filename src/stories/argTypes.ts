16dcbb014f76591289350df5138d57eb994d20f4
Igor Sotnikov <mxtfonlife@mail.ru>
2022-10-03 16:31:08 +0700

    Обновлен argTypes.ts

--- src/stories/argTypes.ts
+++ src/stories/argTypes.ts
@@ -3,17 +3,18 @@ export const argTypes = {
         control: {type: null},
         description: "оборачиваемый элемент"
     },
-
     css: {
         control: {type: null},
         description: "стиль созданный с помощью утилиты `css` из `@emotion/react`"
     },
-
-    colorStyle: {
-        description: "цветовой стиль"
-    },
-
-    disabled: {
-        description: "элемент будет отключен для взаимодействия"
-    }
+    colorStyle: {description: "цветовой стиль"},
+    disabled: {description: "элемент будет отключен для взаимодействия"},
+    label: {description: "подпись"},
+    stickOnHover: {description: "появится эффект \"прилипания\" при наведении на кнопку"},
+    fillable: {description: "кнопка будет заливаться цветом (в соответствии с выбранным стилем) при наведении на неё"},
+    flat: {description: "кнопка будет плоской (без тени)"},
+    borderless: {description: "границы не будут выводиться"},
+    transparent: {description: "фон кнопки будет прозрачным"},
+    style: {description: "стиль, который применится к корневому элементу", control: {type: null}},
+    className: {description: "имя css-класса, который применится к корневому элементу", control: {type: null}}
 };
