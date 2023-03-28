9a988a17b812d2f8ee623feef0b200d9e4661b62
Igor Sotnikov <mxtfonlife@mail.ru>
2023-03-28 12:55:33 +0700

    Поправены типы

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -3,8 +3,6 @@ import React from "react";
 
 export type ColorStyle = 'light' | 'dark' | 'primary' | 'secondary';
 
-//export type BaseCss = SerializedStyles | ((...args: any) => SerializedStyles);
-//export type CssArray = (BaseCss | undefined)[]
 export type Css = SerializedStyles | (SerializedStyles | null | undefined)[];
 
 export type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWithRef<C>["ref"];
@@ -12,8 +10,8 @@ export type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWi
 export type Point = { x: number, y: number };
 
 export type ValueType = string | number;
-export type ItemType = string | number | { [key: string]: string | number };
+export type ItemType = string | number | { [key: string]: any };
 
-export type GetContent = (item: ItemType, contentProp: string) => string | number | null;
+export type GetContent = (item: ItemType, contentProp: string) => string | number | JSX.Element | null;
 export type GetValue = (item: ItemType, valueProp: string, itemIndex: number) => ValueType;
 export type GetSelectable = (item: ItemType, valueProp: string, itemIndex: number) => boolean;
