489c63df12e8a4e6f789e1a7b2c5bcfbfef840de
Игорь Сотников <sotnikov@kemsu.ru>
2024-10-29 15:07:19 +0700

    fix: правка offset

--- src/utils.ts
+++ src/utils.ts
@@ -1,89 +1,76 @@
-import {Point} from "./types";
+import { Point } from "./types"
 
-export function offset(elem : HTMLElement) {
+export function offset(elem: HTMLElement) {
 
-    let offsetLeft = 0;
-    let offsetTop = 0;
-    let el : HTMLElement | null = elem;
+	const rect = elem.getBoundingClientRect()
 
-    do {
-        if (!isNaN(el.offsetLeft)) {
-            offsetLeft += el.offsetLeft;
-        }
-        if (!isNaN(elem.offsetTop)) {
-            offsetTop += el.offsetTop;
-        }
-
-        el = el.offsetParent as HTMLElement;
-
-    } while (el)
-
-    return {
-        offsetLeft, offsetTop
-    }
+	return {
+		offsetTop: rect.top + window.scrollY,
+		offsetLeft: rect.left + window.scrollX
+	}
 
 }
 
-export function toArray(val : unknown) {
-    return Array.isArray(val) ? val : [val];
+export function toArray(val: unknown) {
+	return Array.isArray(val) ? val : [val]
 }
 
-export function debounce(f : (...args: unknown[]) => void, delayMs : number) {
+export function debounce(f: (...args: unknown[]) => void, delayMs: number) {
 
-    let timer: number = 0;
+	let timer: number = 0
 
-    function debounced(...args: unknown[]) {
+	function debounced(...args: unknown[]) {
 
-        if (timer) window.clearTimeout(timer);
+		if (timer) window.clearTimeout(timer)
 
-        timer = window.setTimeout(() => {
-            f(...args)
-            timer = 0
-        }, delayMs);
-    }
+		timer = window.setTimeout(() => {
+			f(...args)
+			timer = 0
+		}, delayMs)
+	}
 
-    debounced.cancel = () => window.clearTimeout(timer);
+	debounced.cancel = () => window.clearTimeout(timer)
 
-    return debounced;
+	return debounced
 }
 
 export function getValueFromIndex(_: unknown, index: number) {
-    return index;
+	return index
 }
 
-export function getTrue(){
-    return true;
+export function getTrue() {
+	return true
 }
 
 export function getElementPositionRelativeTo(element: HTMLElement, targetElement: HTMLElement, targetPosition: Point, targetElementPivot: Point) {
 
-    const elementBCR = element.getBoundingClientRect();
+	const elementBCR = element.getBoundingClientRect()
 
-    const p = {
-        x: lerp(elementBCR.left, elementBCR.right, targetPosition.x),
-        y: lerp(elementBCR.top, elementBCR.bottom, targetPosition.y)
-    };
+	const p = {
+		x: lerp(elementBCR.left, elementBCR.right, targetPosition.x),
+		y: lerp(elementBCR.top, elementBCR.bottom, targetPosition.y)
+	}
 
-    const parentEl = targetElement.offsetParent;
+	const parentEl = targetElement.offsetParent
 
-    if (!parentEl) return null;
+	if (!parentEl) return null
 
-    const parentElBCR = parentEl.getBoundingClientRect();
-    return ({
-        x: p.x - parentElBCR.left - targetElement.clientWidth * targetElementPivot.x,
-        y: p.y - parentElBCR.top - targetElement.clientHeight * targetElementPivot.y
-    }) as Point
+	const parentElBCR = parentEl.getBoundingClientRect()
+	return ({
+		x: p.x - parentElBCR.left - targetElement.clientWidth * targetElementPivot.x,
+		y: p.y - parentElBCR.top - targetElement.clientHeight * targetElementPivot.y
+	}) as Point
 }
 
-export function isPrimitive(arg : unknown) {
-    const type = typeof arg;
-    return arg == null || (type != "object" && type != "function");
+export function isPrimitive(arg: unknown) {
+	const type = typeof arg
+	return arg == null || (type != "object" && type != "function")
 }
 
-export function lerp(start : number, end : number, t : number) {
-    return start * (1 - t) + end * t;
+export function lerp(start: number, end: number, t: number) {
+	return start * (1 - t) + end * t
 }
 
-export function clamp(v : number, a = 0, b = 1) {
-    return Math.min(Math.max(v, a), b);
+export function clamp(v: number, a = 0, b = 1) {
+	return Math.min(Math.max(v, a), b)
 }
